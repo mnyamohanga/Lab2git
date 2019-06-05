cd177b6d876153f25b7b404ede91c75a630682bf
Margaret <margaret2019tech@gmail.com>
2019-06-04 21:24:51 -0400

    HelloWorld

--- src/edu/cscc/HelloWorld.java
+++ src/edu/cscc/HelloWorld.java
@@ -1,5 +1,7 @@
 package edu.cscc;
-
+/* **Margaret Nyamohanga**
+***    Date: 06/04/2019
+* ***   GitLab */
 public class HelloWorld {
     public static void main(String[] arg) {
         System.out.println("Hello World!");
